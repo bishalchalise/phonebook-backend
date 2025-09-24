bf0f8d5d11eb9a81c05b1d6ca1c78d6bc1ecaa25
Bishal Chalise <bishalise@gmail.com>
2025-09-24 14:34:10 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const Person = require("./models/person");
 const morgan = require("morgan");
+
 const app = express();
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.use(express.json());
 app.use(express.static("dist"));
+let persons = [];
 
 morgan.token("body", (request) => {
   return JSON.stringify(request.body);
@@ -41,40 +21,28 @@ app.get("/info", (request, response) => {
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${now}
     </div>`);
-  ß;
 });
 //get all persons
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    console.log("response form here");
+    response.json(persons);
+  });
 });
 //get person by id
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response
-      .status(404)
-      .json({
-        error: "invalid Id",
-      })
-      .end();
-  }
+  });
 });
 
-//delete person by id
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
-});
+// //delete person by id
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = request.params.id;
+//   persons = persons.filter((p) => p.id !== id);
+//   response.status(204).end();
+// });
 
-//generate random ID
-generateId = () => {
-  const id = Math.random().toString(36).substring(2, 7);
-  return String(id);
-};
 //create a person
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -92,13 +60,14 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  });
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+  console.log("person saved");
 });
 
 const unknownEndpoint = (request, response) => {
