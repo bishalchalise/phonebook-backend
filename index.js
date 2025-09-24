8e2cea66fc034de0ea159bcfdbaa25ad7e2daddd
Bishal Chalise <bishalise@gmail.com>
2025-09-24 20:55:09 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const Person = require("./models/person");
 const morgan = require("morgan");
 
 const app = express();
-app.use(express.json());
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted id" });
+  }
+  next(error);
+};
 app.use(express.static("dist"));
+app.use(express.json());
+
 let persons = [];
 
 morgan.token("body", (request) => {
@@ -25,23 +33,32 @@ app.get("/info", (request, response) => {
 //get all persons
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
-    console.log("response form here");
     response.json(persons);
   });
 });
 //get person by id
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-// //delete person by id
-// app.delete("/api/persons/:id", (request, response) => {
-//   const id = request.params.id;
-//   persons = persons.filter((p) => p.id !== id);
-//   response.status(204).end();
-// });
+//delete person by id
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 //create a person
 app.post("/api/persons", (request, response) => {
@@ -75,6 +92,7 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
